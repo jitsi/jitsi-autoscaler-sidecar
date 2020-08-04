659580a358e5f4bb5f47f506145cef1a536ac51e
Aaron van Meerten <aaron.van.meerten@gmail.com>
2020-08-04 12:42:29 -0500

    FEAT: actually poll for status FEAT: validate configuration with envalid

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ import express from 'express';
 import logger from './logger';
 import Poller, { InstanceDetails } from './autoscale_poller';
 import ShutdownHandler from './shutdown_handler';
+import fs from 'fs';
 
+const jwtSigningKey = fs.readFileSync(config.AsapSigningKeyFile);
 const app = express();
 app.use(bodyParser.json());
 
@@ -27,6 +29,10 @@ const shutdownHandler = new ShutdownHandler({
 
 const autoscalePoller = new Poller({
     pollUrl: config.PollingURL,
+    signingKey: jwtSigningKey,
+    asapJwtIss: config.AsapJwtIss,
+    asapJwtAud: config.AsapJwtAud,
+    asapJwtKid: config.AsapJwtKid,
     instanceDetails: instanceDetails,
     shutdownHandler: shutdownHandler,
 });
--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,35 @@
 import * as dotenv from 'dotenv';
+import envalid from 'envalid';
 
 dotenv.config();
 
+const env = envalid.cleanEnv(process.env, {
+    PORT: envalid.num({ default: 6000 }),
+    LOG_LEVEL: envalid.str({ default: 'info' }),
+    POLLING_INTERVAL: envalid.num({ default: 60 }),
+    GRACEFUL_SHUTDOWN_SCRIPT: envalid.str({ default: '/usr/local/bin/graceful_shutdown.sh' }),
+    TERMINATE_SCRIPT: envalid.str({ default: '/usr/local/bin/terminate_instance.sh' }),
+    ASAP_JWT_ISS: envalid.str({ default: 'jitsi-autoscaler-sidecar' }),
+    ASAP_JWT_AUD: envalid.str({ default: 'jitsi-autoscaler' }),
+    POLLING_URL: envalid.str(),
+    INSTANCE_ID: envalid.str(),
+    INSTANCE_METADATA: envalid.json(),
+    ASAP_SIGNING_KEY_FILE: envalid.str(),
+    ASAP_JWT_KID: envalid.str(),
+});
+
 export default {
-    HTTPServerPort: process.env.PORT || 6000,
-    LogLevel: process.env.LOG_LEVEL || 'info',
+    HTTPServerPort: env.PORT,
+    LogLevel: env.LOG_LEVEL,
     // number of seconds to wait between polling for shutdown
-    PollingInterval: Number(process.env.POLLING_INTERVAL || 60),
-    PollingURL: process.env.POLLING_URL || 'http://localhost:3000/sidecar/poll',
-    GracefulShutdownScript: process.env.GRACEFUL_SHUTDOWN_SCRIPT || '/usr/local/bin/graceful_shutdown.sh',
-    TerminateScript: process.env.TERMINATE_SCRIPT || '/usr/local/bin/terminate_instance.sh',
-    InstanceId: process.env.INSTANCE_ID,
-    InstanceMetadata: JSON.parse(process.env.INSTANCE_METADATA || '{}'),
+    PollingInterval: env.POLLING_INTERVAL,
+    PollingURL: env.POLLING_URL,
+    GracefulShutdownScript: env.GRACEFUL_SHUTDOWN_SCRIPT,
+    TerminateScript: env.TERMINATE_SCRIPT,
+    InstanceId: env.INSTANCE_ID,
+    InstanceMetadata: env.INSTANCE_METADATA,
+    AsapSigningKeyFile: env.ASAP_SIGNING_KEY_FILE,
+    AsapJwtKid: env.ASAP_JWT_KID,
+    AsapJwtIss: env.ASAP_JWT_ISS,
+    AsapJwtAud: env.ASAP_JWT_AUD,
 };
